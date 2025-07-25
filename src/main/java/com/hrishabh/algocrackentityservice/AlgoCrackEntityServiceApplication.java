3601059cd6e966309d6474a169b6daa2be063c2d
Hrishabh Joshi <hrishabhjoshi123@gmail.com>
2025-07-25 11:47:14 +0530

    Project created

---
+++ src/main/java/com/hrishabh/algocrackentityservice/AlgoCrackEntityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hrishabh.algocrackentityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoCrackEntityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoCrackEntityServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrishabh/algocrackentityservice/AlgoCrackEntityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrishabh.algocrackentityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoCrackEntityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
